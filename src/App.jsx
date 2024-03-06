a8058ea3a25c470474c620500ac887cf205ba7d7
movsic08 <124447619+movsic08@users.noreply.github.com>
2024-03-06 08:41:44 +0000

    added sidebar menu in small dev in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import { Navbar } from './components';
 
 function App() {
   return (
-    <div className=" bg-slate-50 w-screen h-screen  text-slate-800">
+    <div className=" bg-slate-50 font-poppinsw-screen h-screen  text-slate-800">
       <div className="container ">
         <Navbar />
         <h3 className=" mt-2">React JS + Firebase Realtime DB + Tailwind CSS</h3>
-        <div className="flex w-full items-center justify-between mt-2 border-t-2 border-slate-100 py-2">
+        <div className="flex w-full items-center justify-between mt-2 border-t-2 border-slate-100 py-1">
           <h1>Your to do list</h1>
           <button className=" px-3 py-2 bg-sky-700 text-white rounded-md">Add</button>
         </div>
