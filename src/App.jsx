6e1539f4deac5afc1fa53e0187125ab96a0db81b
movsic08 <124447619+movsic08@users.noreply.github.com>
2024-03-07 09:01:43 +0000

    fixed some nameConventions

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <div className="flex w-full items-center justify-between mt-2 border-t-2 border-slate-100 py-1">
           <h1 className=" font-bold text-2xl text-slate-700">Your to do list</h1>
           <button onClick={openModal} className=" px-2 py-1 bg-sky-700 flex items-center gap-2 text-white rounded-md">
-            <svg className=" w-4 h-4 text-sky-700 bg-white rounded-full  " fill="currentColor " stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg className=" w-4 h-4 text-sky-700 bg-white rounded-full  " fill="currentColor " stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
             </svg>Add</button>
         </div>
--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -11,14 +11,14 @@ const AddTaskModal = ({ isOpen, isCloseModal }) => {
                                 <h1 className=" text-2xl">Add task</h1>
                                 <button onClick={isCloseModal} className="bg-red-600 p-1 duration-500 ease-in-out hover:bg-red-800 rounded-full">
                                     <svg className=" h-4 w-4  text-white " fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M5.152 5.152a1.2 1.2 0 0 1 1.696 0L12 10.303l5.152-5.151a1.2 1.2 0 1 1 1.696 1.696L13.697 12l5.151 5.152a1.2 1.2 0 0 1-1.696 1.696L12 13.697l-5.152 5.151a1.2 1.2 0 0 1-1.696-1.696L10.303 12 5.152 6.848a1.2 1.2 0 0 1 0-1.696Z" clip-rule="evenodd"></path>
+                                        <path fillRule="evenodd" d="M5.152 5.152a1.2 1.2 0 0 1 1.696 0L12 10.303l5.152-5.151a1.2 1.2 0 1 1 1.696 1.696L13.697 12l5.151 5.152a1.2 1.2 0 0 1-1.696 1.696L12 13.697l-5.152 5.151a1.2 1.2 0 0 1-1.696-1.696L10.303 12 5.152 6.848a1.2 1.2 0 0 1 0-1.696Z" clipRule="evenodd"></path>
                                     </svg>
                                 </button>
                             </div>
                             <div className="flex mt-3">
                                 <div className="w-full">
-                                    <label for="first_name" class="block mb-1  font-medium text-gray-900 dark:text-white">Task</label>
-                                    <textarea type="text" id="first_name" class="bg-gray-50 b   order border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What's your task..." required />
+                                    <label htmlFor="first_name" className="block mb-1  font-medium text-gray-900 dark:text-white">Task</label>
+                                    <textarea type="text" id="first_name" className="bg-gray-50 b   order border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What's your task..." required />
                                 </div>
                             </div>
                             <div className="flex w-full items-center justify-end mt-3">
