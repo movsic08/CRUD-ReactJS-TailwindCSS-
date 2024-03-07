e6a4071afb73c900eed9c0bd1c8547fcd3f09bd3
movsic08 <124447619+movsic08@users.noreply.github.com>
2024-03-07 08:07:55 +0000

    basic modal toggling working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 // import './App.css';
 import "./index.css";
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, ToDoItem, AddTaskModal } from './components';
 
+
 function App() {
+
+  const [isAddModalOpen, setAddModalOpen] = useState(false);
+
+  const openModal = () => setAddModalOpen(true);
+  const closeModal = () => setAddModalOpen(false);
+
   return (
     <div className=" bg-slate-100 font-poppinsw-screen h-screen  text-slate-800">
       <div className="container ">
@@ -11,13 +18,13 @@ function App() {
         <h3 className=" mt-2  text-slate-700">React JS + Firebase Realtime DB + Tailwind CSS</h3>
         <div className="flex w-full items-center justify-between mt-2 border-t-2 border-slate-100 py-1">
           <h1 className=" font-bold text-2xl text-slate-700">Your to do list</h1>
-          <button className=" px-2 py-1 bg-sky-700 flex items-center gap-2 text-white rounded-md">
+          <button onClick={openModal} className=" px-2 py-1 bg-sky-700 flex items-center gap-2 text-white rounded-md">
             <svg className=" w-4 h-4 text-sky-700 bg-white rounded-full  " fill="currentColor " stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
             </svg>Add</button>
         </div>
 
-        {/* <AddTaskModal /> */}
+        <AddTaskModal isOpen={isAddModalOpen}  isCloseModal={closeModal}/>
         <ToDoItem />
       </div>
     </div>
--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -1,24 +1,35 @@
+import React from "react";
 
-export default function AddTaskModal() {
+const AddTaskModal = ({ isOpen, isCloseModal }) => {
     return (
         <div >
-            <div className="fixed inset-0 z-50  h-full w-full bg-gray-300 bg-opacity-25 backdrop-blur-sm">
-                <div className="flex items-start justify-center w-full">
-                    <div className="bg-white rounded-md  mt-11 p-2 w-[35%] drop-shadow-sm">
-                        <div className="font-bold w-full flex items-center justify-between text-slate-800">
-                            <h1>Add task</h1>
-                            <button>X</button>
-                        </div>
-                        <div className="flex mt-2">
-                            <div>
-                                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+            {isOpen && (
+                <div className="fixed inset-0 z-50  h-full w-full bg-gray-500 bg-opacity-25 backdrop-blur-sm">
+                    <div className="flex items-start justify-center w-full">
+                        <div className="bg-white rounded-lg  mt-11 p-4 w-[35%] drop-shadow-sm transform transition-all">
+                            <div className="font-bold w-full flex items-center justify-between text-slate-800">
+                                <h1 className=" text-2xl">Add task</h1>
+                                <button onClick={isCloseModal} className="bg-red-600 p-1 duration-500 ease-in-out hover:bg-red-800 rounded-full">
+                                    <svg className=" h-4 w-4  text-white " fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.152 5.152a1.2 1.2 0 0 1 1.696 0L12 10.303l5.152-5.151a1.2 1.2 0 1 1 1.696 1.696L13.697 12l5.151 5.152a1.2 1.2 0 0 1-1.696 1.696L12 13.697l-5.152 5.151a1.2 1.2 0 0 1-1.696-1.696L10.303 12 5.152 6.848a1.2 1.2 0 0 1 0-1.696Z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="flex mt-3">
+                                <div className="w-full">
+                                    <label for="first_name" class="block mb-1  font-medium text-gray-900 dark:text-white">Task</label>
+                                    <textarea type="text" id="first_name" class="bg-gray-50 b   order border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What's your task..." required />
+                                </div>
+                            </div>
+                            <div className="flex w-full items-center justify-end mt-3">
+                                <button className="  bg-sky-600 text-white  font-medium px-3 py-1 rounded-lg">Add</button>
                             </div>
                         </div>
                     </div>
                 </div>
-
-            </div>
+            )}
         </div>
-    )
-}
+    );
+};
+
+export default AddTaskModal;
